ef4788a90f758d366286f916ee4f43e2cb431682
Usuario\charlyeze <charly_cae@hotmail.com>
2024-08-26 23:13:00 -0300

    agregamos data nombre de barreras

---
+++ AppClienteQR/src/data/barreras.js
@@ -0,0 +1,13 @@
+const barreras = {
+    barrera1: "Barrera 1",
+    barrera2: "Barrera 2",
+    barrera3: "Barrera 3",
+    barrera4: "Barrera 4",
+    barreraluz: "Barrera Luz 1",
+    barreraluz2: "Barrera Luz 2",
+    barreraluz3: "Barrera Luz 3",
+    barreraluz4: "Barrera Luz 4",
+    barreraluz5: "Barrera Luz 5",
+};
+
+export default barreras;
