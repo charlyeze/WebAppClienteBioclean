1cb8b8dbf6098bea3a693bad090356a2a4c3895f
Usuario\charlyeze <charly_cae@hotmail.com>
2024-09-01 23:00:29 -0300

    agregamos favicon y logo

--- AppClienteQR/src/components/scan.jsx
+++ AppClienteQR/src/components/scan.jsx
@@ -4,7 +4,16 @@ import { Scanner } from "@yudiel/react-qr-scanner";
 
 const ScanQr = ({ setScanResult }) => {
   const onScan = (result) => result[0] && setScanResult(result[0]?.rawValue);
-  return <Scanner onScan={onScan} />;
+  return (
+    <>
+      <Scanner onScan={onScan} />
+      <img
+        className="w-50 img-fluid position-absolute top-50 start-50 translate-middle"
+        src="/logo.png"
+        alt="Bioclean"
+      ></img>
+    </>
+  );
 };
 
 ScanQr.propTypes = {
